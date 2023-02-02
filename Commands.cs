b28f0a2116b260f249a00527f255a50415ccb3ac
deepchoudhery <decho@microsoft.com>
2023-02-01 23:03:25 -0800

    change args to options

--- Commands.cs
+++ Commands.cs
@@ -1,6 +1,7 @@
 using ScaffoldSpectra;
 using Spectre.Console;
 using Spectre.Console.Cli;
+using System.IO;
 
 public class ScaffoldSettings : CommandSettings
 {
@@ -10,18 +11,28 @@ public class ScaffoldSettings : CommandSettings
 
 public class MinimalApiSettings : ScaffoldSettings
 {
-    [CommandArgument(0, "[ModelFile]")]
-    public string ModelFile { get; set; } = default!;
+    [CommandOption("--modelFile")] 
+    public string? ModelFile { get; set; } = default!;
+    
+    [CommandOption("--endpointsClassName")]
+    public string? EndpointsClassName { get; set; } = default!;
+    
+    [CommandOption("--endpointsFile")]
+    public string? EndpointsFile { get; set; }
+
+    [CommandOption("--dbContext")]
+    public string? DbContext { get; set; }
 }
 
 public class MinimalApiCommand : Command<MinimalApiSettings>
 {
     public override int Execute(CommandContext context, MinimalApiSettings settings)
     {
+        //get model class
         var modelFile = settings.ModelFile;
         if (string.IsNullOrEmpty(modelFile))
         {
-            var allFiles = Helper.GetAllFiles(@"C:\Users\Deep\source\repos\deepchoudhery\ScaffoldSpectra\", ".cs");
+            var allFiles = Helper.GetAllFiles(Directory.GetCurrentDirectory().ToString(), ".cs");
             modelFile = AnsiConsole.Prompt(
                 new SelectionPrompt<string>()
                     .Title("Pick a valid model file")
@@ -29,7 +40,25 @@ public class MinimalApiCommand : Command<MinimalApiSettings>
                     .MoreChoicesText("[grey](Move up and down to reveal more files)[/]")
                     .AddChoices(allFiles));
         }
-        AnsiConsole.MarkupLine($"Model file name - {modelFile}");
+        
+        AnsiConsole.MarkupLine($"`Model file name` - {modelFile}");
+        var modelName = Path.GetFileNameWithoutExtension(modelFile) + "Endpoints";
+
+        //get endpoints class name
+        var endpointsClassName = settings.EndpointsClassName;
+        if (string.IsNullOrEmpty(endpointsClassName))
+        {
+            var className = AnsiConsole.Ask<string>("Give a `class name`:", modelName);
+            endpointsClassName = className;
+        }
+
+        //check for db context
+        if (string.IsNullOrEmpty(settings.DbContext))
+        {
+            var confPrompt = new ConfirmationPrompt("Do you want a `DbContext` scaffolded with the endpoints?");
+            confPrompt.DefaultValue = false;
+            bool wantDbContext = AnsiConsole.Prompt(confPrompt);
+        }
         return 0;
     }
 
